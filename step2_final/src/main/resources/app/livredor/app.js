bfe1200f631e8cad5dbed42b599767d89a3d55b2
karim <karim.benyahia@serli.com>
2019-04-04 11:41:41 +0200

    parcel

--- step2_final/src/main/resources/app/livredor/app.js
+++ step2_final/src/main/resources/app/livredor/app.js
@@ -1,20 +1,38 @@
-import * as angular from 'angular';
+import angular from 'angular';
 import Comment from "./comment/comment";
-import "../style/app.css";
-import "jquery";
+import "../style/app.css";import "jquery";
 
-
-import router from '../livredor/router/router';
-import 'angular-ui-router';
+import CommentsComponent from './comment/comment';
+import uirouter from '@uirouter/angularjs';
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import {default as userServiceName, UserService} from "../service/UserService";
 // Declare livredor level module which depends on views, and core components
-angular.module('app', ['ui.router'])
-  .component(Comment.name, Comment.component)
-  .service(userServiceName, UserService)
-  .config(['$stateProvider', '$urlRouterProvider', router])
+angular.module('app', [uirouter])
+    .component(Comment.name, Comment.component)
+    .service(userServiceName, UserService)
+    .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+
+        const comments = {
+            name: "comments",
+            state: {
+                url: "/comments",
+                views: {
+                    'main@': {
+                        component: CommentsComponent.name
+                    }
+                }
+            }
+        };
+
+        $urlRouterProvider.otherwise("/comments");
+
+        $stateProvider
+            .state(comments.name, comments.state)
+
+    }])
 
 
 angular.bootstrap(document.body, ['app']);
--- step4_final/src/main/resources/app/login/login.ts
+++ step4_final/src/main/resources/app/login/login.ts
@@ -1,20 +1,52 @@
 import {bootstrap, module} from 'angular';
-import "@babel/polyfill";
 import Login from "./login/login";
-
-import router from './router/router';
-import 'angular-ui-router';
-
+//
+import uirouter from '@uirouter/angularjs';
+import LoginComponent from './login/login'
 import {default as userServiceName, UserService} from "../service/UserService";
-import {default as csrfInterceptorName, csrfInterceptor} from "../service/Interceptor";
 
-module('login', ['ui.router'])
+module('login', [
+    uirouter,
+])
     .component(Login.name, Login.component)
     .service(userServiceName, UserService)
-    .factory(csrfInterceptorName, csrfInterceptor)
-    .config(['$httpProvider', function ($httpProvider) {
-        $httpProvider.interceptors.push(csrfInterceptorName)
+    .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+
+        const login = {
+            name: "login",
+            state: {
+                url: "/login",
+                views: {
+                    'main@': {
+                        component: LoginComponent.name
+                    }
+                }
+            }
+        };
+
+        const error = {
+            name: "login.error",
+            state: {
+                url: "/error",
+                views: {
+                    'error@login': {
+                        controller: function ($scope, $timeout, $state) {
+                            $timeout(() => alert("Couple login/password incorrect!"), 100);
+                            $state.go("login");
+                        }
+                    }
+                }
+            }
+        };
+
+
+        $urlRouterProvider.otherwise("/login");
+
+        $stateProvider
+            .state(login.name, login.state)
+            .state(error.name, error.state)
+
     }])
-    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', router])
 ;
 bootstrap(document.body, ['login']);
